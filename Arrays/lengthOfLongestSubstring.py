5d4408c29fe84c1fd171c9bc213a8afc4db4405d
JBond-69 <80713705+JBond-69@users.noreply.github.com>
2025-07-27 18:04:16 +0530

    Create lengthOfLongestSubstring.py
    Longest Substring Without Repeating Characters

---
+++ Arrays/lengthOfLongestSubstring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        len_of_longest_substring = 0 #final required result
+        #len_of_s = len(s)
+        index_of_dup_char = 0
+        char = 0
+        dic = {}
+        while char in range(0,len(s)):
+            if s[char] not in dic or dic[s[char]]<index_of_dup_char:
+                if (char+1)-index_of_dup_char>len_of_longest_substring:
+                    len_of_longest_substring = (char+1)-index_of_dup_char
+                dic[s[char]]=char
+            else:
+                index_of_dup_char = dic[s[char]]+1
+                dic[s[char]]=char
+                temp_len = 0
+            #print(s[char],temp_len,len_of_longest_substring,index_of_dup_char)
+            #print(dic)
+            char+=1
+        return len_of_longest_substring
