0c3df30c59fae61c99a24d3e301a80c26c8a91e4
JBond-69 <80713705+JBond-69@users.noreply.github.com>
2025-07-27 18:06:45 +0530

    Update two_sum.py

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -1,3 +1,6 @@
+# Runtime: 1771 ms, faster than 21% of Python3 submissions
+# Memory Usage: 18 MB, beats 87% of Python3 submissions
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0,len(nums)-1):
